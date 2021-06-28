880472726701bb8df7332bd4c14aaf7384cf627b
Wachira Duangdee <wcrcorp@gmail.com>
2021-06-28 19:47:16 +0700

    Add files via upload

---
+++ app-node-logstash.js
@@ -0,0 +1,17 @@
+#!node
+const winston = require('winston');
+
+const logger = require('winston-logstash-transport').createLogger(null, {
+    application: 'BEMIS-SCHOOL',
+    logstash: { 
+        host: '192.168.4.42', 
+        port: 514 },
+    transports: [
+        new winston.transports.Console(),
+    ]
+})
+
+logger.log('info', 'TIME' + Date.now()  );
+// logger.log('error', 'some message err');
+logger.on('finish', () => process.exit());
+logger.end();
